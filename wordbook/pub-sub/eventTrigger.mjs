1a533721c728b315c52a6c4b9ba5df434b681705
Jun Kim <93kimhyunjun@gmail.com>
2019-12-12 08:33:35 +0900

    feature: add search & toggle event

---
+++ wordbook/pub-sub/eventTrigger.mjs
@@ -0,0 +1,38 @@
+import { dataService } from './dataService.mjs'
+import { options } from './options.mjs'
+
+export const handleToggle = e => {
+  const words = dataService.getWordList()
+  const word = words[e.target.key]
+
+  if (word.status === 'name') {
+    e.target.innerText = word.meaning
+    word.status = 'meaning'
+  } else {
+    e.target.innerText = word.name
+    word.status = 'name'
+  }
+}
+
+const handleAddWord = () => {
+  event.preventDefault()
+
+  const word = ds.setWordFormat(options.nameEl.value, options.meaningEl.value)
+
+  dataService.addWord(word)
+}
+
+const handleSearchWord = e => {
+  if (e.key === 'Enter') {
+    event.preventDefault()
+    const inputVal = options.searchEl.value
+
+    const wordLiElArr = Array.from(options.wordListEl.childNodes)
+    wordLiElArr.forEach(el => {
+      if (el.innerText !== inputVal) el.style.display = 'none'
+    })
+  }
+}
+
+options.btnEl.addEventListener('click', handleAddWord)
+options.searchEl.addEventListener('keypress', handleSearchWord)
