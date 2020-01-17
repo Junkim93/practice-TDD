fc79abcd6f248ef1cf5299f88d5205cb34e4b562
Jun Kim <93kimhyunjun@gmail.com>
2020-01-17 23:18:20 +0900

    init test code

---
+++ ToDo/src/index.test.js
@@ -0,0 +1,9 @@
+const { Renderer, View } = require('./index.js')
+
+/*
+  Given: Input창에 텍스트를 입력했을 때,
+  When: 추가 버튼을 누르면,
+  Then: Input창 밑에 입력한 텍스트가 추가된다.
+*/
+// 인수테스트로 작성 예정
+describe('Renderer의', () => {})
